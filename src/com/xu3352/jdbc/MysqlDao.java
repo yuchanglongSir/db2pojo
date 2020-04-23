2f3eed3c0e40fda56b1f51e75059c7d317c468f6
yuchanglong <971415447@qq.com>
2020-04-23 12:53:41 +0800

    更新mysql工具类

--- src/com/xu3352/jdbc/MysqlDao.java
+++ src/com/xu3352/jdbc/MysqlDao.java
@@ -16,7 +16,7 @@ public class MysqlDao extends AbstractDaoSupport {
 	
 	@Override
 	public List<String> queryAllTables() {
-		return queryAllTables("select * from pg_tables where schemaname ='public'");
+		return queryAllTables("select table_name from information_schema.tables where table_schema='pagenow_open'");
 	}
 
 	@Override
@@ -25,11 +25,11 @@ public class MysqlDao extends AbstractDaoSupport {
 		try {
 			checkDriver();
 			Connection conn = getConn();
-			ResultSet rs = createQuary(conn, "select col.*,des.description FROM information_schema.columns col LEFT JOIN pg_description des ON col.table_name::regclass = des.objoid AND col.ordinal_position = des.objsubid WHERE table_schema = 'public' AND table_name = '"+tableName+"' ORDER BY ordinal_position");
+			ResultSet rs = createQuary(conn, "select col.* FROM information_schema.columns col  WHERE table_schema = 'pagenow_open' AND table_name = '"+tableName+"' ORDER BY ordinal_position");
 			while (rs.next()) {
-				String type = typesConvert(rs.getString(28));
+				String type = typesConvert(rs.getString(8));
 				String javaStyle = StringUtil.javaStyle(rs.getString(4));
-				list.add(new Column(type, rs.getString(4), javaStyle,rs.getString(45)));
+				list.add(new Column(type, rs.getString(4), javaStyle,rs.getString(20),rs.getString(16)));
 			}
 			rs.close();
 			conn.close();
@@ -41,7 +41,7 @@ public class MysqlDao extends AbstractDaoSupport {
 	
 	@Override
 	public String typesConvert(String mysqlType) {
-		if (mysqlType.startsWith("varchar") || mysqlType.startsWith("longtext") || mysqlType.startsWith("text")) {
+		if (mysqlType.startsWith("varchar") || mysqlType.startsWith("longtext") || mysqlType.startsWith("text") || mysqlType.startsWith("char")) {
 			return "String";
 		} else if (mysqlType.startsWith("int4") || mysqlType.startsWith("int2") || mysqlType.startsWith("bigint") || mysqlType.startsWith("tinyint") || mysqlType.startsWith("smallint")) {
 			return "Integer";
@@ -61,7 +61,7 @@ public class MysqlDao extends AbstractDaoSupport {
 	 */
 	public static void main(String[] args) {
 		AbstractDaoSupport dao = new MysqlDao();
-		List<Column>tables = dao.queryColumns("tb_lable");
+		List<Column>tables = dao.queryColumns("pn_page");
 		System.out.println(tables);
 //		List<Column> list = dao.queryColumns(tables.get(0));
 //		for (int i = 0; i < list.size(); i++) {
